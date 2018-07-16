78e11251346da4c1d8ff95759297e014c3deeb7a
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 12:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  
+}
+function destructivelyPrependKitten(name){
+  
+}
+function destructivelyRemoveLastKitten(){
+  
+}
+
+function destructivelyRemoveFirstKitten(){
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  
+}
