8bbc9add4bc3750decf989215688c6c98be0fbf0
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 12:27:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  
+  kittens.splice(0,0,name)
 }
 function destructivelyPrependKitten(name){
-  
+
 }
 function destructivelyRemoveLastKitten(){
-  
+
 }
 
 function destructivelyRemoveFirstKitten(){
-  
+
 }
 
 function appendKitten(name) {
-  
+
 }
 
 function prependKitten(name){
-  
+
 }
 
 function removeLastKitten(){
-  
+
 }
 
 function removeFirstKitten(){
-  
+
 }
